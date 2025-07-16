660a0e4b09a900aa3df027d535038c8109e0175b
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-07-16 19:15:46 +0700

    add proram, course

--- src/Pages/Program.jsx
+++ src/Pages/Program.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+// src/Pages/Program.jsx
+import ProgramCard from "../Components/Molecules/ProgramCard";
+import { programsData } from "../Data/program";
 
-function Program() {
+const Program = () => {
   return (
-    <div>
-      <h1>Isi Table Program HIMTI</h1>
-    </div>
-  )
-}
+    // UBAH DARI GRADASI MENJADI BG-GRAY-50
+    <div className="bg-gray-50">
+      <section className="container mx-auto px-5 py-24">
+        {/* Judul Halaman */}
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-4xl text-3xl font-bold title-font mb-4 text-secondary">
+            Program Unggulan Kami
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-600">
+            Pilih jalur belajarmu dan mulailah perjalanan untuk menjadi seorang profesional di bidang teknologi.
+          </p>
+        </div>
 
-export default Program
+        {/* Grid untuk Kartu Program */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {programsData.map((program) => (
+            <ProgramCard
+              key={program.id}
+              icon={program.icon}
+              title={program.title}
+              description={program.description}
+              link={program.link}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Program;
