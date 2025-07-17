c8000b1e1d4abeb9077f8cedc437a9f854b93029
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-07-17 22:36:31 +0700

    Add PWA Fitur

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,61 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'HIMTI Learning Group',
+        short_name: 'HIMTI Learn',
+        description: 'Platform komunitas untuk mahasiswa Teknik Informatika UMT untuk belajar bersama.',
+        theme_color: '#8B5CF6', 
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'himti-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'himti-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+     screenshots: [
+          {
+            src: 'manifest-web.png', // path ke gambar di folder public
+            sizes: '1280x720', // ukuran gambar dalam piksel
+            type: 'image/png',
+            form_factor: 'wide', // 'wide' untuk desktop/tablet
+            label: 'Tampilan Desktop Aplikasi'
+          },
+          {
+            src: 'manifest-mobile.png',
+            sizes: '540x720',
+            type: 'image/png',
+            form_factor: 'narrow', // 'narrow' untuk mobile
+            label: 'Tampilan Mobile Aplikasi'
+          }
+        ]
+      }
+    })
+  ],
 })
